a601ed3553c9a77d209f0547395d7c1d877641fe
Taliha Arif <taliha.arif@systemsltd.com>
2022-12-05 16:45:51 +0500

    computed and watchers

--- src/components/ChildInjector.vue
+++ src/components/ChildInjector.vue
@@ -2,6 +2,8 @@
   <div class="hello">
     <h1> This is a child getting data from parent via injector.</h1>
     {{childMessage}}
+
+    <p>Has register in course : {{ registerCourse }}</p>
   </div>
 </template>
 
@@ -9,6 +11,33 @@
 export default {
   name: 'ChildInjector',
   inject: ['childMessage'],
+  data(){
+    return {
+      grade: 10,
+      student: {
+        name: 'John Doe',
+        courses: [
+          'DSA',
+          'DBA',
+          'SQA'
+        ]
+      }
+    }
+  },
+  watch: {
+    grade(newGrade, oldGrade) {
+      console.log('old and new Grade', newGrade,  oldGrade)
+    }
+  },
+  mounted() {
+    this.grade = this.grade + 1
+  },
+  computed: {
+    registerCourse() {
+      return this.student.courses.includes('SQA')  ? 'Yes' : 'No'
+    }
+  }
+  
 }
 </script>
 
